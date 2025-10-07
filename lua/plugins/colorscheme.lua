a85ccc4c00da35dd23b019a72079e9d0bb554e46
Amandeep563 <amandeepbollampalli@gmail.com>
2025-10-07 22:04:46 +0530

    update nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,3 @@
--- return {
---   "navarasu/onedark.nvim",
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require("onedark").setup({
---       style = "darker",
---     })
---     -- Enable theme
---     require("onedark").load()
---   end,
--- }
 return {
   "shaunsingh/nord.nvim",
   priority = 1000, -- Ensures it loads before other plugins
