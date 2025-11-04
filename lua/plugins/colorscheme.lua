df61ee59051145a9766e992e4e8c44bd4866d887
Amandeep563 <amandeepbollampalli@gmail.com>
2025-11-04 23:44:25 +0530

    update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,28 @@
--- return {
---   {
---     "gbprod/nord.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---       require("nord").setup({})
---       vim.cmd.colorscheme("nord")
---     end,
---   },
--- }
 return {
   {
-    "mhartington/oceanic-next",
-    name = "oceanic-next",
+    "thesimonho/kanagawa-paper.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("OceanicNext")
+      require("kanagawa-paper").setup({
+        undercurl = true,
+        transparent = false,
+        gutter = false,
+        diag_background = true,
+        dim_inactive = false,
+        terminal_colors = true,
+        cache = false,
+        styles = {
+          comment = { italic = true },
+          functions = { italic = false },
+          keyword = { italic = false, bold = false },
+          statement = { italic = false, bold = false },
+          type = { italic = false },
+        },
+      })
+
+      vim.cmd.colorscheme("kanagawa-paper-ink") -- dark
+      -- vim.cmd.colorscheme("kanagawa-paper-canvas") -- light
     end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "OceanicNext",
-    },
   },
 }
